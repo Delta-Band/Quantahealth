ff16a9875948af8284d3b34a05c8f1cb8249193a
Ofer Sarid <ofer@delta.band>
2022-01-26 15:07:01 +0200

    added gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import '../styles/globals.css';
 import theme from '../theme';
 import { PageLayout, SplashScreen } from '../components';
+import TagManager from 'react-gtm-module';
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: 'GTM-NG9D255'
+    });
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <PageLayout {...pageProps}>
